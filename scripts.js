c049e123336b9d9b1fae6dd80dff292c8a82fdf2
Patrick-Sun <patrick3843@gmail.com>
2021-07-06 17:58:35 -0400

    working

--- scripts.js
+++ scripts.js
@@ -1,3 +1,24 @@
 function flip(ele) {
 	ele.classList.toggle('is_flipped')
 };
+
+function order_name() {
+	$("#deck li").sort(name_sort).appendTo('#deck');
+	document.getElementById("button_name").classList.add('button_active')
+	document.getElementById("button_date").classList.remove('button_active')
+
+}
+
+function order_date() {
+	$("#deck li").sort(date_sort).appendTo('#deck');
+	document.getElementById("button_date").classList.add('button_active')
+	document.getElementById("button_name").classList.remove('button_active')
+}
+
+function name_sort(a, b){
+    return a.id.localeCompare(b.id);
+}
+
+function date_sort(a, b){
+    return a.dataset.order.localeCompare(b.dataset.order);
+}
